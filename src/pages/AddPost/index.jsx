2f7873d4ccb6d0a2f039a5e68bcf4dcbfa0a432f
Yurii Cerber <yuriicerber@gmail.com>
2023-07-04 00:45:51 +0000

    comit1

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -33,7 +33,7 @@ export const AddPost = () => {
       setImageUrl(data.url)
     } catch (err) {
       console.warn(err);
-      alert('Помилка при загрузці файлівв');
+      alert('Помилка при загрузці файлів');
     }
   };
 
